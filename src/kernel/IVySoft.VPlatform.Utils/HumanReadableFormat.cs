37a535906cd5ced118878c6a8442ecfa389a4bf2
Vadim Malyshev <lboss75@gmail.com>
2020-05-05 11:38:32 +0300

    update

---
+++ src/kernel/IVySoft.VPlatform.Utils/HumanReadableFormat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IVySoft.VPlatform.Utils
+{
+    public static class HumanReadableFormat
+    {
+        // Returns the human-readable file size for an arbitrary, 64-bit file size 
+        // The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
+        public static string GetBytesReadable(long len)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
+
+            return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+    }
+}
