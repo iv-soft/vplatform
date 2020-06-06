7cee9564e4222474cff31ae8881c55e6574d08a1
Vadim Malyshev <lboss75@gmail.com>
2020-06-06 13:04:48 +0300

    size in hr

--- src/kernel/IVySoft.VPlatform.Utils/HumanReadableFormat.cs
+++ src/kernel/IVySoft.VPlatform.Utils/HumanReadableFormat.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace IVySoft.VPlatform.Utils
 {
     public static class HumanReadableFormat
     {
+        static string[] sizes = { "", "K", "M", "G", "T" };
         // Returns the human-readable file size for an arbitrary, 64-bit file size 
         // The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
         public static string GetBytesReadable(long len)
         {
-            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
             int order = 0;
             while (len >= 1024 && order < sizes.Length - 1)
             {
@@ -19,6 +20,19 @@ namespace IVySoft.VPlatform.Utils
             }
 
             return String.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+        public static long Parse(string len)
+        {
+            for(int order = sizes.Length; order > 0; --order)
+            {
+                if(len.EndsWith(sizes[order - 1]))
+                {
+                    return (long)(double.Parse(len, CultureInfo.InvariantCulture) * Math.Pow(1024, order - 1));
+
+                }
+            }
+
+            throw new Exception($"Invalid lenght format {len}");
         }
     }
 }
