93853ce86abd4c58bc0401d9a718b4c4052d5777
Vadim Malyshev <lboss75@gmail.com>
2021-04-04 13:07:44 +0300

    Add WebAPI utils

---
+++ src/webapi/IVySoft.VPlatform.WebAPI.Utils/JwtUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace IVySoft.VPlatform.WebAPI.Utils
+{
+    public static class JwtUtils
+    {
+        public static int GetUserId(ClaimsPrincipal user)
+        {
+            return int.Parse(user.Claims.Single(
+                    y => y.Type == Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti).Value);
+        }
+    }
+}
