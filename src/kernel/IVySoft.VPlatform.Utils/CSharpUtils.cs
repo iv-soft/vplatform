a6be79961307fc8714117f46f31ade1aaa7a5033
Vadim Malyshev <lboss75@gmail.com>
2021-04-25 08:30:05 +0300

    Add CSharpUtils

---
+++ src/kernel/IVySoft.VPlatform.Utils/CSharpUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IVySoft.VPlatform.Utils
+{
+    public static class CSharpUtils
+    {
+        public static string TypeShortName(string type_name)
+        {
+            switch (type_name)
+            {
+                case "System.Boolean": return "bool";
+                case "System.String": return "string";
+                case "System.Int32": return "int";
+                default: return type_name;
+            }
+        }
+    }
+}
